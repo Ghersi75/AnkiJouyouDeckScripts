00e46fc2c37bf4987327c3af112ba0960195b2ce
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-10-05 18:25:11 -0400

    Removed print and reduced sleep

--- kanjiApi.py
+++ kanjiApi.py
@@ -7,14 +7,14 @@ async def fetch(session, url):
         content = await response.text()
         if response.status == 200 and response.content_type == 'application/json':
             # print(f"Content for {url}:\n{content}")
-            print(f"Done with: {url}")
+            # print(f"Done with: {url}")
             if content:
                 # print(json.loads(content))
                 return json.loads(content)
             return await response.json()
         if response.status == 429:
             print("Sleep zzzz")
-            await asyncio.sleep(2)
+            await asyncio.sleep(1)
             return await fetch(session, url)
         else:
             print(f"Error with URL {url}. Status: {response.status}, Content-Type: {response.content_type}")

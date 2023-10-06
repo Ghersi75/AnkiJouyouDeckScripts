acdcf8299ffade869f08faae5ed6ca1999556df6
Ghersi Doci <34931324+Ghersi75@users.noreply.github.com>
2023-10-05 21:28:27 -0400

    Added sorting grade by frequency

--- findBestSentence.py
+++ findBestSentence.py
@@ -3,3 +3,18 @@ from utils import loadJson
 
 if __name__ == "__main__":
   grade1Sentences = loadJson("./_grade1Sentences.json")
+  frequencyKtoN = loadJson("./_frequencyKanjiToNumber.json")
+  frequencyNtoK = loadJson("./_frequencyNumberToKanji.json")
+
+  # Rank sentences based on the kanji used in them
+  s = []
+  for kanji, entry in grade1Sentences.items():
+    s.append({
+      kanji: entry
+    })
+
+  s = sorted(s, key=lambda x: int(frequencyKtoN[list(x.keys())[0]]))
+
+  for e in s:
+    kanji = list(e.keys())[0]
+    print("Kanji: ", kanji, " Ranking: ", frequencyKtoN[kanji])
